c1a44f3927e2aef864e18d958c03edf7cbbe9cd7
FebryanAkt <febryan08022006@gmail.com>
2024-09-26 20:48:47 +0700

    Percobaan 1 ternary operator

--- PemilihanBilangan10.java
+++ PemilihanBilangan10.java
@@ -7,13 +7,20 @@ public class PemilihanBilangan10 {
     System.out.print("Masukkan sebuah angka: ");
     int angka = sc.nextInt();
 
-    if (angka % 2==0)
-    {
-        System.out.println("Angka " + angka + " termasuk bilangan genap");
-    }
-    else
-    {
-        System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-    }
+    // if (angka % 2==0)
+    // {
+    //     System.out.println("Angka " + angka + " termasuk bilangan genap");
+    // }
+    // else
+    // {
+    //     System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+    // }
+
+    String hasil;
+
+    hasil = (angka % 2==0) ? "Agka " +angka+ " termasuk bilangan genap" : "Angka " +angka+ " termasuk bilangan ganjil";
+
+    System.out.println(hasil);
+    
     }
 }
